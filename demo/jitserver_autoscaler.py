208867d7e93f277ea6e284a4eba8014f4c092b03
NithSelv <nithushan.selvanesan@mail.utoronto.ca>
2022-04-13 08:46:29 -0400

    Finalized timing issues

--- demo/jitserver_autoscaler.py
+++ demo/jitserver_autoscaler.py
@@ -7,6 +7,7 @@ import requests
 import json
 import signal
 import os
+import struct
 
 processes = []
 
@@ -14,12 +15,25 @@ processes = []
 
 # It will transmit port numbers over port 2000 to whoever is reading them. It's just a 4byte integer. Decode it and typecast to int to read it.
 
-def send_new_port(port, msg):
+def send_new_port(port, jit_port, metric_ports, load):
    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    host = socket.gethostname()
    ip = socket.gethostbyname(host)
-   sock.connect((ip, port))
-   sock.send(str(msg).encode())
+   try: 
+      sock.connect((ip, port))
+   except socket.error:
+      sock.close()
+      return
+   msg = struct.pack("!i", jit_port)
+   num_bytes = 0
+   while num_bytes < len(msg):
+      num_bytes = sock.send(msg[num_bytes:])
+   for x in metric_ports:
+      if x in load.keys():
+         msg = struct.pack("!i", load[x])
+         num_bytes = 0
+         while num_bytes < len(msg):
+            num_bytes = sock.send(msg[num_bytes:])
    sock.close()
 
 def get_metrics():
@@ -55,34 +69,36 @@ def kill_processes(*args):
    
 
 def main(jitserver_path, prometheus_path):
-   jit_port = 38400
+   jit_port = 3840
    metric_port = 7390
    https_port = 7391
    metric_ports = [7390]
    processes.append(make_jitserver_instance(jitserver_path, jit_port, metric_port, https_port))
    print("Created JITServer Instance No: "+str(len(processes)))
    print("Sending original port for listening on port 2000!")
-   send_new_port(2000, jit_port)
+   send_new_port(2000, jit_port, metric_ports, {metric_port: 0})
    processes.append(start_prometheus(prometheus_path))
    print("Started Prometheus instance!")
    time.sleep(5)
    signal.signal(signal.SIGINT, kill_processes)
    while True: 
-      time.sleep(5)
+      time.sleep(10)
       load = get_metrics();
+      new = False
       for i in range(len(metric_ports)):
-         if len(load.keys()) != 0 and load[metric_ports[i]] > 1:
+         if len(load.keys()) != 0 and (metric_ports[i] in load.keys()) and load[metric_ports[i]] >= 5 and len(metric_ports) < 5 and not new:
             jit_port += 1
             metric_port += 2
             https_port += 2
             processes.append(make_jitserver_instance(jitserver_path, jit_port, metric_port, https_port))
+            new = True
             time.sleep(5)
-            print("Created JITServer Instance No: "+str(len(metric_ports)))
-            print("Please send some requests to port: "+str(jit_port))
             metric_ports.append(metric_port)
-            send_new_port(2000, metric_port)
-         if (len(load.keys()) != 0):
+            send_new_port(2000, jit_port, metric_ports, load)
+         if (len(load.keys()) != 0) and (metric_ports[i] in load.keys()):
             print("JITServer Instance No: "+str(i+1)+" has "+str(load[metric_ports[i]])+" clients")
+      if new:
+         print("Created JITServer Instance No: "+str(len(metric_ports)))
 
 if __name__ == "__main__":
    if len(sys.argv) > 2 and os.path.exists(sys.argv[1]) and os.path.exists(sys.argv[2]):
--- demo/jitserver_request_launcher.py
+++ demo/jitserver_request_launcher.py
@@ -8,6 +8,7 @@ import json
 import signal
 import os
 import threading
+import struct
 
 # This script is for firing compilation requests to jitserver and trying to keep the load at around 5 requests
 
@@ -15,18 +16,40 @@ c = threading.Lock()
 p = threading.Lock()
 f = threading.Lock()
 ports = []
+loads = []
 processes = []
 finished = False
 
 def recvmsg(sock):
    global ports
    global finished
+   global loads
    while True:
       try: 
+         f.acquire()
+         if finished:
+            f.release()
+            sock.close()
+            return
+         f.release()
          connection, _ = sock.accept()
-         new_port = int(connection.recv(4).decode('utf-8'))
+         x = b''
+         while len(x) < 4:
+            x = x + connection.recv(4-len(x))
+         new_port = struct.unpack("!i", x)[0]
+         c.acquire()
+         temp = loads[:]
+         size = len(ports)+1
+         c.release()
+         temp.append(0)
+         for i in range(size):
+            x = b''
+            while len(x) < 4:
+               x = x + connection.recv(4-len(x))
+            temp[i] = struct.unpack("!i", x)[0]
          c.acquire()
          ports.append(new_port)
+         loads = temp
          c.release()
          p.acquire()
          print("Received a new port: "+str(new_port))
@@ -43,37 +66,51 @@ def recvmsg(sock):
          return
    
    
-def sendreq(java_path):
+def sendreq(java_path, load):
    global ports
    global finished
+   global loads
    global processes
+   original_load = load
    while True:
-      f.acquire()
-      if finished:
-         f.release()
-         return
-      f.release()
       c.acquire()
-      ports_copy = list(set(ports))
+      ports_copy = ports[:]
+      loads_copy = loads[:]
       c.release()
-      for x in ports_copy:
-         process = sendcmd(java_path, x)
-         processes.append(process)
-      p.acquire()
-      print("Fired a round of requests!")
-      p.release()
-      time.sleep(10)
+      maxload = max(max(loads_copy), 5)
+      for i in range(len(ports_copy)):
+         if loads_copy[i] >= 5:
+            num_iterations = 0
+         else:
+            num_iterations = min(maxload-loads_copy[i], load)
+         for j in range(num_iterations):
+            process = sendcmd(java_path, ports_copy[i])
+            processes.append(process)
+         load -= num_iterations
+         if (load == 0):
+            f.acquire()
+            finished = True
+            f.release()
+            return
+         c.acquire()
+         loads_copy[i] = 4
+         c.release()
+      if load < original_load:
+         p.acquire()
+         print("Fired a round of requests!")
+         p.release()
+      time.sleep(20)
 
 def sendcmd(java_path, port):
    args = [java_path]
    args.append("-XX:+UseJITServer")
-   args.append("-XX:+JITServerPort="+str(port))
+   args.append("-XX:JITServerPort="+str(port))
    args.append("-Xjit:count=1"+86*"0")
    args.append("-version")
    process = subprocess.Popen(args, shell=False, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, close_fds=True)
    return process
 
-def main(java_path):
+def main(java_path, load):
    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    host = socket.gethostname()
    ip = socket.gethostbyname(host)
@@ -81,7 +118,7 @@ def main(java_path):
    sock.settimeout(100)
    sock.listen()
    t1 = threading.Thread(target=recvmsg, args=(sock,))
-   t2 = threading.Thread(target=sendreq, args=(java_path,))
+   t2 = threading.Thread(target=sendreq, args=(java_path,load,))
 
    t1.start()
    c.acquire()
@@ -103,7 +140,7 @@ def main(java_path):
    print("Killed all leftover processes!")
 
 if __name__ == "__main__":
-   if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
-      main(sys.argv[1])
+   if len(sys.argv) > 2 and os.path.exists(sys.argv[1]):
+      main(sys.argv[1], int(sys.argv[2]))
    else:
-      print("Please specify the path to the java binary!")
+      print("Please specify the path to the java binary and the number of loads requests!")
